10e977ec2706629d54b2cc654b274f318f67daf6
Zildjian Garcia <zackyization@gmail.com>
2024-07-26 17:54:46 +0700

    Update basic.cy.js to cater to correct content

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,16 +1,11 @@
-describe('empty spec', () => {
+describe('Homepage tests', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+  it('displays the correct hero section content', () => {
+    // Check for the hero section's main content
+    cy.get('.foreground-content img[alt="Lion Statue Image"]').should('be.visible')
+    cy.get('h1.jumbotron').contains('Hall 3.')
+    cy.get('.foreground-content p').contains('Nanyang Technological University, Singapore')
   })
 })
